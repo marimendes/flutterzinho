704910c75d8b88d175e781d630bf7fff95eff3c1
Mariana Mendes <mare.s.mendes@gmail.com>
2020-01-22 16:55:00 -0300

    Configura a tela de lista

--- lib/main.dart
+++ lib/main.dart
@@ -23,9 +23,9 @@ class HomePage extends StatefulWidget {
 
   HomePage() {
     items = [];
-    items.add(Item(title: "Item 1", done: false));
-    items.add(Item(title: "Item 2", done: true));
-    items.add(Item(title: "Item 3", done: false));
+    items.add(Item(title: "Passagem", done: false));
+    items.add(Item(title: "Passaporte", done: true));
+    items.add(Item(title: "Airbnb", done: false));
   }
   @override
   _HomePageState createState() => _HomePageState();
@@ -42,7 +42,13 @@ class _HomePageState extends State<HomePage> {
       body: ListView.builder(
         itemCount: widget.items.length,
         itemBuilder: (BuildContext context, int index) {
-          return Text(widget.items[index].title);
+          final item = widget.items[index];
+          return CheckboxListTile(
+            title: Text(item.title),
+            key: Key(item.title),
+            value: item.done,
+            onChanged: (value) {},
+          );
         },
       ),
     );
