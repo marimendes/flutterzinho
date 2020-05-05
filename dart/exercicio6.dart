4322452e8353864b23c82316bb1f5d396789d731
Mariana Mendes <mare.s.mendes@gmail.com>
2020-05-05 10:16:31 -0300

    Dart - exercicio 6

---
+++ dart/exercicio6.dart
@@ -0,0 +1,12 @@
+import 'dart:io';
+
+main(){
+
+final lista = ['Ana', 'Lia', 'Gui'];
+// final = constante, não da pra adicionar mais
+// pode alterar e remover
+// se tiver o const, não pode nem ser alterado
+lista.add('Rebeca');
+print(lista);
+
+}
