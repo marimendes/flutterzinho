86a21bea471d75c8ca933d8988829e43ff56ca06
Mariana Mendes <mare.s.mendes@gmail.com>
2020-05-11 16:05:48 -0300

    Instalando o pacote INTL e configurando a data

--- expenses/lib/main.dart
+++ expenses/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:expenses/models/transaction.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 main() => runApp(ExpensesApp());
 
@@ -78,13 +79,13 @@ class MyHomePage extends StatelessWidget {
                         Text(tr.title,
                           style: TextStyle(
                             fontWeight: FontWeight.bold,
-                            fontSize: 20,
+                            fontSize: 16,
                           ),
                         ),
-                        Text(tr.date.toString(),
+                        Text(
+                          DateFormat('d MMM y').format(tr.date),
                           style: TextStyle(
                             color: Colors.grey,
-                            fontSize: 16,
                           ),
                         ),
                       ],
